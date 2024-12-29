95ffc5cb9a367c7d220a5cbca1e6a1a105b6b84b
DimaSh888 <dimon.shipicin@gmail.com>
2024-12-29 17:09:29 +0200

    Add files via upload

---
+++ style.py
@@ -0,0 +1,19 @@
+styleproj='''
+QWidget{
+background-color:#00e650;
+}
+QPushButton{
+background-color: #ff5672 ;
+border-radius: 20px ;
+border:1px;
+width:100px;
+height:50px;
+font-size:15px;
+}
+QLabel{
+font-size:15px;
+}
+
+
+
+'''
