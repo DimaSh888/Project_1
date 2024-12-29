8b0122f3770f947307621fdc370e8a1e779ff0b2
DimaSh888 <dimon.shipicin@gmail.com>
2024-12-29 17:08:10 +0200

    Update main_project_1.py

--- main_project_1.py
+++ main_project_1.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui
+from style import *
 
 app=QApplication([])
 
@@ -10,20 +11,18 @@ win.resize(800,400)
 win.setWindowTitle('Herbarium')
 win.show()
 
+win.setWindowIcon(QtGui.QIcon("icon.ico"))
+win.setStyleSheet(styleproj)
+
 label_p=QLabel('')
-label_p.setFixedSize(400,400)
 label_n=QLabel('Гербарій екзотичних квітів:')
 label_per=QLabel('Перемикач картинок:')
-label_p.hide()
-pixmapimage=QPixmap("1fl.jpg")
-wpic,hpic=label_p.width(),label_p.height()
-pixmapimage=pixmapimage.scaled(400,400,Qt.KeepAspectRatio)
-label_p.setPixmap(pixmapimage)
-label_p.show()
-
+label_n.setStyleSheet(styleproj)
+label_per.setStyleSheet(styleproj)
 btn_n=QPushButton("Наступна")
 btn_l=QPushButton("Минула")
-
+btn_l.setStyleSheet(styleproj)
+btn_n.setStyleSheet(styleproj)
 w1=QVBoxLayout()
 w2=QVBoxLayout()
 h1=QHBoxLayout()
@@ -40,24 +39,31 @@ win.setLayout(h1)
 i=1
 def change_plu():
     global i
-    
-    pixmapimage=QPixmap(str(i)+"fl.jpg")
-    label_p.setPixmap(pixmapimage)
-    pixmapimage=pixmapimage.scaled(400,400,Qt.KeepAspectRatio)
-    if i==6:
-        i=1
     i=i+1
+    pixmapimage=QPixmap(str(i)+"fl.jpg")
+    pixmapimage=pixmapimage.scaled(400,400,Qt.KeepAspectRatio)
+    label_p.setPixmap(pixmapimage)
+    if i==5:
+        i=0
 btn_n.clicked.connect(change_plu)
+
 def change_min():
     global i
-    
     pixmapimage=QPixmap(str(i)+"fl.jpg")
-    label_p.setPixmap(pixmapimage)
     pixmapimage=pixmapimage.scaled(400,400,Qt.KeepAspectRatio)
-    if i==0:
-        i=5
+    label_p.setPixmap(pixmapimage)
+    if i==1:
+        i=6
     i=i-1
 btn_l.clicked.connect(change_min)
 
+label_p.setFixedSize(400,400)
+label_p.hide()
+wpic,hpic=label_p.width(),label_p.height()
+pixmapimage=QPixmap(str(i)+"fl.jpg")
+pixmapimage=pixmapimage.scaled(wpic,hpic,Qt.KeepAspectRatio)
+label_p.setPixmap(pixmapimage)
 
+
+label_p.show()
 app.exec()
